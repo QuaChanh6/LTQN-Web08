80ee5e007af7fbe9e49608462b632fc2181f61f8
nga <lethiquynhngab@gmail.com>
2023-01-25 19:59:55 +0700

    up:

--- MISA.Web08.QTKD.LTQN.API/Controllers/EmployeesController.cs
+++ MISA.Web08.QTKD.LTQN.API/Controllers/EmployeesController.cs
@@ -259,77 +259,7 @@ namespace MISA.Web08.QTKD.API.Controllers
             }
         }
 
-        [HttpPost("File/{employeeCode}")]
-        [Obsolete]
-        public IActionResult FileAttach([FromForm] IFormFile file, [FromRoute] string employeeCode)
-        {
 
-            string[] Scopes = { Google.Apis.Drive.v3.DriveService.Scope.Drive };
-            string ApplicationName = "ManageEmployee";
-
-            UserCredential credential;
-            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-
-                // Thông tin về quyền truy xuất dữ liệu của người dùng được lưu ở thư mục token.json
-                string credPath = "token.json";
-
-                // Yêu cầu người dùng xác thực lần đầu và thông tin sẽ được lưu vào thư mục token.json
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                GoogleClientSecrets.Load(stream).Secrets,
-                Scopes,  // Quyền truy xuất dữ liệu của người dùng
-                "user",
-                CancellationToken.None,
-                new FileDataStore(credPath, true)).Result;
-
-                Console.WriteLine("Credential file saved to: " + credPath);
-
-                // Tạo ra 1 dịch vụ Drive API - Create Drive API service với thông tin xác thực và ApplicationName
-                var driveService = new Google.Apis.Drive.v3.DriveService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = ApplicationName,
-                });
-
-
-                // ID thư mục file, các bạn thay bằng ID của các bạn khi chạy
-                var folderId = "1hnkn1I9SgMX2Vn2lf0M3GkGTbpe5luM1";
-                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-                {
-                    Name = "hoctoantap.jpg",
-
-                    // Thư mục chưa file
-                    Parents = new List<string>{folderId}
- 
-
-                };
-
-                // Đường dẫn file trong thiết bị của bạn, dùng để upload lên Goolge Drive
-                string pathFile = "hoctoantap.com.jpg";
-
-
-                Google.Apis.Drive.v3.FilesResource.CreateMediaUpload request;
-                using (var streamFile = file.OpenReadStream())
-                {
-                    request = driveService.Files.Create(fileMetadata, streamFile, null);
-
-                    // Cấu hình thông tin lấy về là ID
-                    request.Fields = "id";
-
-                    // thực hiện Upload
-                    request.Upload();
-                }
-
-                // Trả về thông tin file đã được upload lên Google Drive
-                var fileOutPut = request.ResponseBody;
-
-                Console.WriteLine("File ID: " + fileOutPut.Id);
-                //Name a;
-                //a.call();
-                return StatusCode(StatusCodes.Status200OK);
-
-            }
-        }
 
     }
     
